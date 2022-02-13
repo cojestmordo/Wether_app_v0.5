1dd7831357bc9b6b5fdd74b08012dd97f129feed
cojestmordo <lordmada2012@gmail.com>
2022-02-13 23:58:13 +0100

    soe tests

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://whats-the-weather-in-london.herokuapp.com/
+ test
--- index.js
+++ index.js
@@ -1,78 +1,20 @@
-const express = require ('express');
-const axios = require ('axios');
-const bodyParser = require('body-parser');
+const express = require('express');
 const env = require('dotenv');
-const nodemailer = require('nodemailer')
+const morgan = require('morgan')
+
+const home = require('./Routes/home.js');
+const api = require('./Routes/api.js');
+const post = require('./Routes/post.js')
 
 env.config();
-
 const app = express();
+const port = process.env.PORT || 5654;
 
-const port = process.env.PORT || 4444;
+app.use(morgan('combined'))
+app.use('/', home);
+app.use('/api', api);
+app.use('/send', post)
 
-
-
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static('public'));
-
-app.get('/',  (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.sendFile(__public + '/index.html');
-});
-
-app.get('/api', async (req, res) =>{
-    const api_key = process.env.API_KEY
-
-    const config = {
-        method: 'get',
-        url: `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`
-    }
-    let api_res = await axios(config)
-    res.send(api_res.data)
-})
-
-app.post("/send", (req, res) => {
-    var newID = req.body.ID;
-    console.log(req.body.newID)
-    res.redirect('/')
-    async function main() {
-    
-        const api_key = process.env.API_KEY
-        const mail_key = process.env.MAIL_KEY
-        const config = {
-            method: 'get',
-            url: `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`
-        }
-        let api_res = await axios(config)
-        let weather = api_res.data.current.condition.text;
-    
-        let transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: 'sjopel75@gmail.com', 
-            pass: mail_key, 
-          },
-        });
-      
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-          from: '"Fred Foo" <foo@example.com>', // sender address
-          to: req.body.newID, // list of receivers
-          subject: "Hello ✔", // Subject line
-          text: `Weather in London is: ${weather}`, // plain text body
-          html: `<b>Weather in London is: ${weather}</b>`, // html body
-        });
-      
-        console.log("Message sent: %s", info.messageId);
-      
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    }
-      
-      main().catch(console.error);
-
-})
-
-app.listen (port, () => {
-    console.log(`listening on port ${port}.`)
+app.listen(port, () => {
+  console.log(`listening on port ${port}.`)
 })
